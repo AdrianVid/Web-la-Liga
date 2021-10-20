d3c5fd1832d6b6b3c25e175b8e09217d2735287d
Adrian Vidaurre <adrianvid16@gmail.com>
2021-10-20 08:38:42 +0200

    dos tablas estadisticas

--- estadisticas.js
+++ estadisticas.js
@@ -1,5 +1,4 @@
-let tbody = document.getElementById("tbody");
-console.log(data.matches);
+estadPartidos(data.matches);
 function estadPartidos(partidos) {
   let arrEquipos = [];
 
@@ -19,12 +18,12 @@ function estadPartidos(partidos) {
         id: partidos[i].homeTeam.id,
         nombre: partidos[i].homeTeam.name,
         goles: partidos[i].score.fullTime.homeTeam,
-        partido: 1,
+        partidos: 1,
       };
       arrEquipos.push(crearEquipo1);
     } else {
       equipoLocal.goles += partidos[i].score.fullTime.homeTeam;
-      equipoLocal.partido++;
+      equipoLocal.partidos++;
     }
 
     let equipoVisitante = arrEquipos.find(function (equipo) {
@@ -39,18 +38,117 @@ function estadPartidos(partidos) {
         id: partidos[i].awayTeam.id,
         nombre: partidos[i].awayTeam.name,
         goles: partidos[i].score.fullTime.awayTeam,
-        partido: 1,
+        partidos: 1,
       };
       arrEquipos.push(crearEquipo2);
     } else {
       equipoVisitante.goles += partidos[i].score.fullTime.awayTeam;
-      equipoVisitante.partido++;
+      equipoVisitante.partidos++;
+    }
+
+    for (let j = 0; j < arrEquipos.length; j++) {
+      arrEquipos[j].avg = arrEquipos[j].goles / arrEquipos[j].partidos;
     }
   }
-  for (let j = 0; j < arrEquipos.length; j++) {
-    arrEquipos[j].avg = arrEquipos[j].goles / arrEquipos[j].partido;
-  }
-  console.log(arrEquipos);
+  ordenaravg(arrEquipos);
+  crearTablaAvg(arrEquipos);
+}
+function ordenaravg(arrEquipos) {
+  arrEquipos.sort(function (b, a) {
+    if (a.avg > b.avg) {
+      return 1;
+    }
+    if (a.avg < b.avg) {
+      return -1;
+    }
+    return 0;
+  });
 }
 
-estadPartidos(data.matches);
+function crearTablaAvg(array) {
+  let tbody = document.getElementById("tbody");
+
+  for (let i = 0; i < array.length; i++) {
+    if (i == 5) {
+      break;
+    }
+    let fila = document.createElement("tr");
+    let escudo = document.createElement("img");
+    escudo.src = `https://crests.football-data.org/${array[i].id}.svg`;
+
+    let celda1 = document.createElement("td");
+    celda1.append(escudo, `${array[i].nombre}`);
+    let celda2 = document.createElement("td");
+    celda2.innerHTML = array[i].goles;
+    let celda3 = document.createElement("td");
+    celda3.innerHTML = array[i].partidos;
+    let celda4 = document.createElement("td");
+    celda4.innerHTML = array[i].avg.toFixed(2);
+
+    fila.append(celda1, celda2, celda3, celda4);
+    tbody.append(fila);
+  }
+}
+partidosVisitantes(data.matches);
+function partidosVisitantes(partidos) {
+  let visitantes = [];
+
+  for (let i = 0; i < partidos.length; i++) {
+    if (partidos[i].status !== "FINISHED") {
+      continue;
+    }
+    let equipoAway = visitantes.find(function (awayTeam) {
+      if (partidos[i].awayTeam.id === awayTeam.id) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if (equipoAway === undefined) {
+      let crearEquipo3 = {
+        id: partidos[i].awayTeam.id,
+        nombre: partidos[i].awayTeam.name,
+        golesEnContra: partidos[i].score.fullTime.homeTeam,
+        partidosVis: 1,
+      };
+      visitantes.push(crearEquipo3);
+    } else {
+      equipoAway.golesEnContra += partidos[i].score.fullTime.homeTeam;
+      equipoAway.partidosVis++;
+    }
+  }
+  ordenarVis(visitantes);
+  crearTablaVis(visitantes);
+}
+function ordenarVis(visitantes) {
+  visitantes.sort(function (a, b) {
+    if (a.golesEnContra > b.golesEnContra) {
+      return 1;
+    }
+    if (a.golesEnContra < b.golesEnContra) {
+      return -1;
+    }
+    return 0;
+  });
+}
+function crearTablaVis(array2) {
+  let tbody2 = document.getElementById("tbody2");
+  for (let i = 0; i < array2.length; i++) {
+    if (i == 5) {
+      break;
+    }
+    let fila = document.createElement("tr");
+    let escudo = document.createElement("img");
+    escudo.src = `https://crests.football-data.org/${array2[i].id}.svg`;
+
+    let celda1 = document.createElement("td");
+    celda1.append(escudo, `${array2[i].nombre}`);
+    let celda2 = document.createElement("td");
+    celda2.innerHTML = array2[i].golesEnContra;
+    let celda3 = document.createElement("td");
+    celda3.innerHTML = array2[i].partidosVis;
+
+    fila.append(celda1, celda2, celda3);
+    tbody2.append(fila);
+  }
+}
